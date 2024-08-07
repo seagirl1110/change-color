820317790f58559b634bd89ff82309f0e21ca7e0
Marina Elizarova <seagirl1110@gmail.com>
2024-08-07 14:56:18 +0300

    feat: add change color

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState } from 'react';
 import './App.css';
+import Form from './components/form';
 
 function App() {
-  return <></>;
+  const [color, setColor] = useState('');
+
+  function updateColor(newColor) {
+    setColor(newColor);
+  }
+
+  const styles = {
+    width: '100vw',
+    height: '100vh',
+    backgroundColor: color,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
+  return (
+    <div style={styles}>
+      <Form colorFunction={updateColor} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/form/index.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+function Form({ colorFunction }) {
+  const [color, setColor] = useState('');
+
+  function handleChange(newColor) {
+    setColor(newColor);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    colorFunction(color);
+  }
+
+  const stylesForm = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: '10px',
+  };
+
+  const stylesInput = {
+    display: 'block',
+    width: '235px',
+    height: '48px',
+  };
+
+  const stylesBtn = {
+    width: '100px',
+    height: '28px',
+  };
+
+  return (
+    <form style={stylesForm} onSubmit={handleSubmit}>
+      <input
+        type="color"
+        value={color}
+        onChange={(event) => handleChange(event.target.value)}
+        style={stylesInput}
+      />
+      <button style={stylesBtn} type="submit">
+        change color
+      </button>
+    </form>
+  );
+}
+
+export default Form;
